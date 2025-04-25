8f6ec33aa5fbbcbc9e4330215525f71c2d37fb9a
Fernando Brocco <fernandobrcc@hotmail.com>
2025-04-24 21:00:19 -0300

    Locale Settings configuration

--- app/src/main/java/br/edu/utfpr/helloworld/calculaimc_pm45s_2025_1/MainActivity.kt
+++ app/src/main/java/br/edu/utfpr/helloworld/calculaimc_pm45s_2025_1/MainActivity.kt
@@ -1,6 +1,5 @@
 package br.edu.utfpr.helloworld.calculaimc_pm45s_2025_1
 
-import android.icu.text.DecimalFormat
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
@@ -8,67 +7,80 @@ import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import java.text.DecimalFormat
+import java.text.NumberFormat
+import java.util.Locale
 import kotlin.math.pow
-
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var etPeso: EditText
-    private lateinit var etAltura: EditText
-    private lateinit var tvResultado: TextView
-    private lateinit var btCalcular: Button
-    //private lateinit var btLimpar: Button
+    private lateinit var etPeso : EditText
+    private lateinit var etAltura : EditText
+    private lateinit var tvResultado : TextView
+    private lateinit var btCalcular : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        etPeso = findViewById(R.id.etPeso)
-        etAltura = findViewById(R.id.etAltura)
-        tvResultado = findViewById(R.id.tvResultado)
-        btCalcular = findViewById(R.id.btCalcular)
-        //btLimpar = findViewById(R.id.btLimpar)
+        etPeso = findViewById( R.id.etPeso )
+        etAltura = findViewById( R.id.etAltura )
+        tvResultado = findViewById( R.id.tvResultado )
+        btCalcular = findViewById( R.id.btCalcular )
 
-        btCalcular.setOnClickListener{
+        btCalcular.setOnClickListener {
             btCalcularOnClick()
         }
 
-        btCalcular.setOnLongClickListener {
-            Toast.makeText(this,"Botao para calcular o IMC", Toast.LENGTH_LONG).show()
-            true
+        btCalcular.setOnLongClickListener{
+            Toast.makeText( this, getString(R.string.msg_click_long), Toast.LENGTH_LONG ).show()
+            false
         }
 
-        //btLimpar.setOnClickListener{
-        //    btLimparOnClick()
-        //}
+    } //fim do onCreate()
 
-    }//fim do onCreate
-
-    public fun btLimparOnClick(view : View) {
+    public fun btLimparOnClick( view : View ) {
         etPeso.setText( "" )
         etAltura.setText( "" )
-        tvResultado.setText( "0.0" )
+        tvResultado.setText( getString( R.string.zeros ) )
         etPeso.requestFocus()
     }
 
     private fun btCalcularOnClick() {
-        if (etPeso.text.toString().equals("")){
-            etPeso.error = "Campo deve ser preenchido"
-            //Toast.makeText(this,"Campo peso deve ser preenchido",Toast.LENGTH_LONG).show()
+
+        if ( etPeso.text.toString().equals( "" ) ) {
+            etPeso.error = "Campo peso deve ser preenchido"
             return
         }
-        if (etAltura.text.toString().equals("")){
-            //Toast.makeText(this,"Campo peso deve ser preenchido",Toast.LENGTH_LONG).show()
-            etAltura.error = "Campo deve ser preenchido"
+
+
+        if ( etAltura.text.toString().equals( "" ) ) {
+            etAltura.error = "Campo Altura deve ser preenchido"
             return
         }
+
+
         val peso = etPeso.text.toString().toDouble()
         val altura = etAltura.text.toString().toDouble()
 
-        val imc = peso / altura.pow(2)
+        var imc = 0.0
 
-        val df = DecimalFormat("0.00")
+        imc = calculaIMC(peso,altura,Locale.getDefault().language)
 
-        tvResultado.text = df.format(imc)
+        val numberFormat = NumberFormat.getNumberInstance( Locale.getDefault() )
+        val df = numberFormat as DecimalFormat
+        tvResultado.text = df.format( imc )
     }
+
+
+    companion object{
+        fun calculaIMC(peso: Double, altura: Double, locale: String):Double{
+            if (locale.equals( "en") ) {
+                return 703 * peso / altura.pow( 2 )
+            } else {
+                return  peso / altura.pow( 2 )
+            }
+        }
+    }
+
 } // fim da MainActivity
